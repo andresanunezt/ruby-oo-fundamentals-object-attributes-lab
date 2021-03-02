6aa11e6c892f8c50d63d04982ed7e493c4c79f70
Andrés Nuñez <andresanunez98@gmail.com>
2021-03-02 10:55:55 -0500

    commit tree 5bcc281a90a3fb45f86537e7c1542fbcb45baaaa

---
+++ lib/dog.rb
@@ -0,0 +1,30 @@
+# require 'pry'
+class Dog
+
+# def initialize(name)
+#     @name = name
+#     # binding.pry
+# end
+
+def name=(name)
+    @name = name
+    # binding.pry
+end
+# binding.pry
+def name
+   @name
+end
+
+
+def breed=(breed)
+    @breed = breed
+end
+
+def breed
+    @breed
+end
+
+
+
+end
+
